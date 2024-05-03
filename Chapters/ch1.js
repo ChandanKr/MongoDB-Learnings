549af317eb2826a8c88af1c4b0370295be42c2e1
ChandanKr <chandan2606kr@gmail.com>
2024-05-04 04:56:48 +0530

    Chapter 1: Managing Database and Collection - "Show, Create and Delete"

---
+++ Chapters/ch1.js
@@ -0,0 +1,19 @@
+//! Chapter 1: Managing Database and Collection - "Show, Create and Delete"
+
+//* "mongosh" => to start/connect the database.
+
+//* "show dbs" / "show databases" => to show all the databases.
+
+//* "use <database-name>" => to switch the database if it is already created OR create a new database if it's not there already.
+// Please note that if there is not a single collection in database, it will not show on "show dbs"
+
+//* "db.dropDatabase()" => to delete the current database you are in.
+
+//* "show collections" => to show all the collections in the database in current database.
+
+//* "db.createCollection('<collection-name>')" => to create a new collection.
+
+//* "db.<collection-name>.find({})" => to see the documents (data) inside the collection.
+
+//* "db.<collection-name>.drop()" => to delete the collection.
+
